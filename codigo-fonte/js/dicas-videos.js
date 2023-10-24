9d2ad5ba44a2028b9747b54ef1d9b2a22bdc46d6
Israel Moreira <86491370+Railom96@users.noreply.github.com>
2023-10-24 12:58:38 -0300

    Att
    Estreitamento de termos de busca para as matérias e tratamento de erro 403 na busca de vídeos.

--- codigo-fonte/js/dicas-videos.js
+++ codigo-fonte/js/dicas-videos.js
@@ -1,7 +1,9 @@
-function carregarVideosDoYouTube() {
+function buscarVideosDoYouTube() {
     var divVideos = document.getElementById("videosYoutube");
     var query = "educação financeira";
-    var url = "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDi0LELx8_YR4Ykt0y9AGGQkg4kUQBCycg&part=snippet&q=" + query + "&maxResults=4&type=video";
+    var apiKey = "AIzaSyCelOGg2nRUZamOwDYdTACjdQIloWCYgCg";
+    var url = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet&q=${query}&maxResults=4&type=video`;
+
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url);
     xhr.onload = function () {
@@ -12,21 +14,25 @@ function carregarVideosDoYouTube() {
                     var item = response.items[i];
                     var videoId = item.id.videoId;
                     var videoTitle = item.snippet.title;
-                    var videoThumbnail = item.snippet.thumbnails.medium.url;
                     var videoElement = document.createElement("div");
                     videoElement.innerHTML = `
-            <iframe width="530" height="300" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
-            <h3>${videoTitle}</h3>
-          `;
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
+                        <h3>${videoTitle}</h3>
+                    `;
                     divVideos.appendChild(videoElement);
                 }
             } else {
                 divVideos.innerHTML = "Nenhum vídeo encontrado.";
             }
+        } else if (xhr.status === 403) {
+           
+            divVideos.innerHTML = "Limite de busca da API excedido.";
         } else {
             console.error("Erro ao buscar vídeos do YouTube.");
         }
     };
     xhr.send();
 }
-carregarVideosDoYouTube();
+
+
+buscarVideosDoYouTube();
