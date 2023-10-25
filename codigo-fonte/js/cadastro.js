a40244360077692e170f38e18b97610e2c235d8d
Victor Ballestrini <vsballestrini@gmail.com>
2023-10-24 23:59:03 -0300

    Back-end login e cadastro

---
+++ codigo-fonte/js/cadastro.js
@@ -0,0 +1,27 @@
+function createUser(event) {
+    event.preventDefault(); 
+
+    const form = event.target;
+    const name = form.elements.name.value;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    const user = {
+        name,
+        email,
+        password
+    };
+
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    users.push(user);
+    localStorage.setItem('users', JSON.stringify(users));
+
+    form.reset();
+
+    alert('Usuário criado com sucesso!');
+
+    window.location.href = 'login.html';
+}
+
+const form = document.querySelector('sign-up-form');
+form.addEventListener('submit', createUserOnLocalStorage);
---
+++ codigo-fonte/js/login.js
@@ -0,0 +1,39 @@
+function loginUser(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+
+    let currentUser = null;
+    let userFound = false;
+
+    for (const element of users) {
+        const user = element;
+        if (user.email === email && user.password === password) {
+            currentUser = user;
+            userFound = true;
+            break;
+        }
+    }
+
+    if (userFound) {
+        localStorage.setItem('currentUser', JSON.stringify(currentUser));
+        localStorage.setItem('isLogged', true);
+        window.location.href = 'main.html';
+    } else {
+        alert('Email ou senha inválidos!');
+    }
+}
+
+function logoutUser() {
+    localStorage.removeItem('currentUser');
+    localStorage.setItem('isLogged', false);
+    window.location.href = 'login.html';
+}
+
+const form = document.querySelector('#login-form');
+form.addEventListener('submit', loginUser);
+
